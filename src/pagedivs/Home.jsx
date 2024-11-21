10c03eb3432d9fbb6e18028b1c7581f13212466c
SBhima <emailshivank@gmail.com>
2024-11-21 16:50:21 -0500

    Wording Fix

--- src/pagedivs/Home.jsx
+++ src/pagedivs/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
             <h1 className={styles.homeTitle} >WELCOME</h1>
             <div className='titleBar' > </div>
             <p className={styles.homeFlavorText1} >We don't know what's at the edge of universe, but I'd like to imagine that my portfolio website is out there somewhere.</p>
-            <p className={styles.homeFlavorText2} >Click the big red planet (the one that says "Start Here!") to get started. (Feel free to drag around and zoom in and out, it's interactive).</p>
+            <p className={styles.homeFlavorText2} >Click the big white and blue planet (the one that says "Start Here!") to get started. (Feel free to drag around and zoom in and out, it's interactive).</p>
         </div>
     )
 }
